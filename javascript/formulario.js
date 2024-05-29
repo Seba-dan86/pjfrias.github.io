895ca4038152c2811aa37338c86edf6a23967c35
Sebastian D'Andrea <93914745+Seba-dan86@users.noreply.github.com>
2024-05-29 00:26:05 -0300

    falta importar los datos del form a comprobante y ya estaria

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -2,9 +2,10 @@
 
 
 
-const btn=document.getElementById('btn');
+
 
 /* HAY QUE VALIDAR LOS DATOS INGRESADOS */
+const btn=document.getElementById('btn');
 const img = document.querySelector('.msgError');
 const nombre=document.getElementById('nombre');
 const apellido=document.getElementById('apellido');
@@ -12,7 +13,7 @@ const email=document.getElementById('email');
 const telefono=document.getElementById('telefono');
 const select=document.getElementById('service');
 const padre=document.getElementById('datos');
-const data=nombre.value + " " + apellido.value + " " + email.value + " " + telefono.value + " "+ select.value;
+
 
 
 
@@ -20,34 +21,41 @@ btn.addEventListener("click", function (e) {
 
     if (validar()){
         mostrarTicket();
+        let data= nombre.value +apellido.value +email.value +telefono.value +select.value ;
+
         nombre.style.borderColor='black';
-    apellido.style.borderColor='black';
-    email.style.borderColor='black';
-    telefono.style.borderColor='black';
+        apellido.style.borderColor='black';
+        email.style.borderColor='black';
+        telefono.style.borderColor='black';
         btn.disabled = true;
         nombre.disabled = true;
         apellido.disabled = true;
         email.disabled = true;
         telefono.disabled = true;
 
+
+
     }else{
         mostrarError();
+
     }
 });
 
 
 function validar(){
     if(nombre.value==''||apellido.value==''||email.value==''||telefono.value==''||select.value ==''){
-      nombre.style.borderColor='red';
-      apellido.style.borderColor='red';
-      email.style.borderColor='red';
-      telefono.style.borderColor='red';
-      nombre.placeholder='Debe ingresar un nombre';
-      apellido.placeholder='Debe ingresar un apellido';
-      email.placeholder='Debe ingresar un email valido';
-      telefono.placeholder='Debe ingresar numero de telefono';
-         return false;
+        nombre.style.borderColor='red';
+        nombre.placeholder='Debe ingresar un nombre';
+        apellido.style.borderColor='red';
+        apellido.placeholder='Debe ingresar un apellido';
+        email.style.borderColor='red';
+        email.placeholder='Debe ingresar un email valido';
+        telefono.style.borderColor='red';
+        telefono.placeholder='Debe ingresar numero de telefono de 10 digitos';
+
+        return false;
     }
+
     /* REGEX*/
     let validarNombre= /^[a-zA-Z\W]{1,20}$/;
     let validarApellido= /^[a-zA-Z\W]{1,20}$/;
@@ -60,9 +68,9 @@ function validar(){
     if ( validarNombre.test(nombre.value)) {
         nombre.style.borderColor='green';
 
-
     } else {
 
+        nombre.style.borderColor='red';
         return false;
     }
 
@@ -70,22 +78,23 @@ function validar(){
         apellido.style.borderColor='green';
     } else {
 
-
+        apellido.style.borderColor='red';
         return false;
     }
 
     if (emailPattern.test(email.value)) {
         email.style.borderColor='green';
-        } else {
+    } else {
 
-
+        email.style.borderColor='red';
         return false;
     }
 
-    if(validarTelefono.test(telefono.value)) {
+    if(telefono.value.length == 10 && validarTelefono.test(telefono.value)) {
         telefono.style.borderColor='green';
     }else{
 
+        telefono.style.borderColor='red';
         return false;
     }
     return true;
@@ -131,12 +140,16 @@ function mostrarTicket(){
     pie.setAttribute('class','datosTexto');
     pie.innerHTML='Gracias por elegirnos ! En breve nos contactaremos para su cita';
 
-    let aceptar= document.createElement("a");
+    let aceptar = document.createElement('button');
     padre.appendChild(aceptar);
-    aceptar.innerText='Descargar Comprobante';
+    aceptar.innerText='Aceptar';
 
-    aceptar.setAttribute('download');
-    aceptar.setAttribute('href','../html/comprobante.html');
+    let ancla= document.createElement('a');
+    padre.appendChild(ancla);
+    ancla.innerText='Descargar Comprobante';
+
+
+    ancla.setAttribute('href','../html/comprobante.html');
 
 
 
@@ -144,26 +157,26 @@ function mostrarTicket(){
 
 
     aceptar.addEventListener("click", function (e) {
-    nombre.disabled=false;
-    apellido.disabled=false;
-    email.disabled=false;
-    telefono.disabled=false;
-    btn.disabled=false;
-    nombre.value='';
-    apellido.value='';
-    email.value='';
-    telefono.value='';
+        nombre.disabled=false;
+        apellido.disabled=false;
+        email.disabled=false;
+        telefono.disabled=false;
+        btn.disabled=false;
+        nombre.value='';
+        apellido.value='';
+        email.value='';
+        telefono.value='';
 
-    padre.style.visibility="hidden";
-    padre.removeChild(encabezado);
-    padre.removeChild(aceptar);
-    padre.removeChild(br);
-    padre.removeChild(br2);
-    padre.removeChild(infoNombre);
-    padre.removeChild(infoEmail);
-    padre.removeChild(infoTelefono);
-    padre.removeChild(infoServicio);
-    padre.removeChild(pie);
+        padre.style.visibility="hidden";
+        padre.removeChild(encabezado);
+        padre.removeChild(aceptar);
+        padre.removeChild(br);
+        padre.removeChild(br2);
+        padre.removeChild(infoNombre);
+        padre.removeChild(infoEmail);
+        padre.removeChild(infoTelefono);
+        padre.removeChild(infoServicio);
+        padre.removeChild(pie);
     });
 
 
@@ -183,4 +196,4 @@ function mostrarError(){
 }
 
 
-
+export {data} ;
--- javascript/mostrarDatos.js
+++ javascript/mostrarDatos.js
@@ -1,21 +1,13 @@
+import { data } from "./formulario.js";
 
-
-const nombre=document.getElementById("nombre").value;
-
-const apellido=document.getElementById("apellido").value;
-
-const email=document.getElementById("email").value;
-
-const telefono=document.getElementById("telefono").value;
-
-const service=document.getElementById("service").value;
 
 
 
 const Pdf=document.getElementById("confirmacion");
 let descargarPdf=document.createElement("p");
 Pdf.appendChild(descargarPdf);
-descargarPdf.textContent = mostrarTicket();
+descargarPdf.innerHTML = data;
 
+console.log(data)
 
 
